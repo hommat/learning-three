90b22d7fc3b2435b58ffc986a3a43732ea155ea2
hommat <zmateusz1111@gmail.com>
2020-02-27 15:52:05 +0100

    Complete getting started

--- src/gettingStarted.js
+++ src/gettingStarted.js
@@ -1,4 +1,36 @@
-import { Scene } from 'three';
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshBasicMaterial,
+  Mesh
+} from 'three';
 
 const scene = new Scene();
-console.log(scene);
+const renderer = new WebGLRenderer();
+const camera = new PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.append(renderer.domElement);
+
+const geometry = new BoxGeometry();
+const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+scene.position.z = -5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
